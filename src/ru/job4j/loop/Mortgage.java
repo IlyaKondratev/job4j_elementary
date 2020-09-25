54b4edd8445b4ac914af80914996d46f812d2422
IlyaKondratev <ilya.kondratev@phystech.edu>
2020-09-25 23:12:18 +0300

    5.7. Ипотека [#368973]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 1;
+        double rem = amount;
+        while ((rem + (rem * percent / 100)) > salary) {
+            rem = rem + (rem * percent / 100) - salary;
+            year++;
+        }
+        return year;
+    }
+}
